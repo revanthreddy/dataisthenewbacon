c6f1977e4d7e9add69995b6120b87f5c28daa191
revanth <revanth.reddy@gmail.com>
2015-06-05 11:35:09 -0500

    added cors and aws

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var server = require('http').createServer(app);
 var http = require('http');
 var path = require('path');
 var async = require('async');
-
+var cors = require('cors');
+app.use(cors());
 // Setup Port to Use for Server Communication
 server.listen(3000);
 
@@ -24,23 +25,61 @@ app.use(express.methodOverride());
 app.use(app.router);
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 //    console.log(__dirname);
 //    res.sendfile(__dirname + '/index.html');
     return res.send("DATA IS THE NEW BACON");
 });
 
 
-app.get('/data', function(req, res) {
+app.get('/data', function (req, res) {
 //    console.log(__dirname);
 //    res.sendfile(__dirname + '/index.html');
-    
+
     var data = [];
-    
-    for(var i = 0 ; i < 10 ; i++){
-        
-            data.push(Math.random());
-            
+
+    for (var i = 0; i < 10; i++) {
+
+        data.push(Math.random());
+
     }
     return res.send(data);
 });
+
+
+app.get('/data', function (req, res) {
+//    console.log(__dirname);
+//    res.sendfile(__dirname + '/index.html');
+
+    var data = [];
+
+    for (var i = 0; i < 10; i++) {
+
+        data.push(Math.random());
+
+    }
+    return res.send(data);
+});
+
+app.get('/sample', function (req, res) {
+    var db = new AWS.DynamoDB();
+    var data = [];
+    db.scan({
+        TableName : "tb_channel_data",
+        Limit : 50
+    }, function(err, data) {
+        if (err) { console.log(err); return; }
+        
+        var items = data.Items;
+        for (var i in items) {
+            data.push();
+            console.log(items[i].name.S);
+            console.log(items[i].value.N);
+            console.log(items[i].timestamp.N);
+        }
+        return res.send(data);
+    });
+    
+});
+
+
