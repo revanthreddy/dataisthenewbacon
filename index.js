43a8dc866c945d91da5fa8ce9b1cce51d9ccbd2c
revanth <revanth.reddy@gmail.com>
2015-06-05 09:52:05 -0500

    bacon data

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var app = express();
 var server = require('http').createServer(app);
 var http = require('http');
 var path = require('path');
-
+var async = require('async');
 
 // Setup Port to Use for Server Communication
 server.listen(3000);
 
 var AWS = require('aws-sdk');
-//AWS.config.loadFromPath('/opt/apps/properties/awscred.json');
+AWS.config.loadFromPath('/opt/apps/properties/awscred.json');
 
 
 // App is tied to Express.  These are Express settings.
@@ -27,5 +27,20 @@ app.use(app.router);
 app.get('/', function(req, res) {
 //    console.log(__dirname);
 //    res.sendfile(__dirname + '/index.html');
-    return res.send("DATA IS THE NEW BACON")
+    return res.send("DATA IS THE NEW BACON");
+});
+
+
+app.get('/data', function(req, res) {
+//    console.log(__dirname);
+//    res.sendfile(__dirname + '/index.html');
+    
+    var data = [];
+    
+    for(var i = 0 ; i < 10 ; i++){
+        
+            data.push([Math.random() , (new Date()).getTime()]);
+            
+    }
+    return res.send(data);
 });
